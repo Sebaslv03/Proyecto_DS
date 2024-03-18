04eddf221bbaf4ba22976add37982344c8366aae
Sebaslv03 <selopez@estudiantec.cr>
2024-03-17 23:00:03 -0600

    1?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola, como estan
+          Richi playo.
         </p>
         <a
           className="App-link"
