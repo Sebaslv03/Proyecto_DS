4e8aafa7586ab0607642c8e87dfc6f2f3f99fcab
Sebaslv03 <selopez@estudiantec.cr>
2024-03-17 23:02:32 -0600

    1.1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Richi playo.
+          Prueba Amplify
         </p>
         <a
           className="App-link"
